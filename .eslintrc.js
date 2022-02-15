635bedaad96acdf043a0e53ed5be54ac5b79615d
LuckyScholar <837416656@qq.com>
2022-02-15 14:26:40 +0800

    first commit

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,18 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true,
-  },
-  extends: ["plugin:vue/essential", "eslint:recommended", "@vue/prettier"],
-  parserOptions: {
-    parser: "babel-eslint",
-  },
-  rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
-  },
-};
+    root: true,
+    env: {
+        node: true,
+    },
+    extends: ["plugin:vue/essential", "eslint:recommended", "@vue/prettier"],
+    parserOptions: {
+        parser: "babel-eslint",
+    },
+    rules: {
+        "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
+        "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
+        semi: "off",
+        "prettier/prettier": "off",
+        "no-unused-vars": "off",
+        indent: ["error", 4],
+    },
+}
